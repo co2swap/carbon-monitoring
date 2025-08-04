82782904cdf6ff1f7b4622c90109556430977b1c
co2swap <co2swapofficial@gmail.com>
2025-08-05 00:05:11 +0530

    Delete carbon_utility_agent directory

--- carbon_utility_agent/config/emission_weights.py
+++
@@ -1,6 +0,0 @@
-weights = {
-    'emissions': 1.5,
-    'cost': 1.0,
-    'compliance': 2.0,
-    'renewable': 1.2
-}
--- carbon_utility_agent/utility.py
+++
@@ -1,5 +0,0 @@
-def compute_utility(state, weights):
-    return (-weights['emissions'] * state['emissions']
-            -weights['cost'] * state['cost']
-            +weights['compliance'] * state['compliance']
-            +weights['renewable'] * state['renewable'])
