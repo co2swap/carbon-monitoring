cec0ff565221f9edd935891a3a7fb2de90efeb0e
co2swap <co2swapofficial@gmail.com>
2025-08-05 00:04:47 +0530

    Delete carbon_dashboard directory

--- carbon_dashboard/emissions_calc.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def calculate_emissions():
-    df = pd.read_csv("data/carbon_data.csv")
-    df["emissions"] = df["value"] * df["emission_factor"]
-    by_scope = df.groupby("scope")["emissions"].sum().reset_index()
-    return df, by_scope
