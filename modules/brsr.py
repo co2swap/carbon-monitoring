2d43f9a1f8d8a1e2d397d9a79598b7feb5a72cf3
co2swap <co2swapofficial@gmail.com>
2025-08-07 02:16:04 +0530

    Delete modules directory

--- modules/brsr.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-def export_brsr_format(scope_totals):
-    st.subheader("Download BRSR Format Output")
-    brsr_df = scope_totals.rename(columns={"scope": "Scope", "emissions": "Total Emissions (tCO2e)"})
-    csv = brsr_df.to_csv(index=False).encode("utf-8")
-    st.download_button("Download BRSR Report (CSV)", csv, "brsr_output.csv", "text/csv")
